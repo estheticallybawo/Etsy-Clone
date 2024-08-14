12f21e729bcc8cbb2dabf9f4f7c53c3ae6f5f38e
Esther <estheticallybawo@gmail.com>
2024-08-14 09:21:18 -0700

    header styling attempt

--- src/app/page.js
+++ src/app/page.js
@@ -10,13 +10,12 @@ import { ImGift } from "react-icons/im";
 
 const page = () => {
   return (
-    <nav >
-      
+    <span id='Header' className='flex space-x-4'>
+    <nav  className='flex space-x-4'>
       <div className="w-full max-w-xl flex mx-auto p-20 text-xl">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className='Logo'>
           <Link href ="/">
-            <img src ="esty logo.png"/>
+            <img className= 'flex space-x-4'src ="esty logo.png"/>
           </Link>
         
           <h3 className='flex space-x-4'><TfiMenu /> Categories </h3>
@@ -31,9 +30,9 @@ const page = () => {
           </Link>
             <input
                 type="text"
-                className="w-full placeholder-gray-400 text-gray-900 p-4"
+                className="w-full placeholder-gray-400 text-gray-900 p-4 flex space-x-4"
                 placeholder="Search for anything"/>
-            <button className="bg-orange-600 text-white p-4"><IoSearchSharp /></button>
+            <button className="bg-orange-600 text-white flex space-x-4 p-4"><IoSearchSharp /></button>
 
             <Link href="">
             <h3 className='flex space-x-4 '><ImGift /> Gift Mode</h3>
@@ -44,11 +43,9 @@ const page = () => {
             <Link href=""> <h3>Fashion Finds</h3></Link>
             <Link href=""> <h3>Registry</h3></Link>
         </div>
-  
-      </div>
-      </div>
+        </div>
       </nav>
-
+      </span>
   )
 }
 
