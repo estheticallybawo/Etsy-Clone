ab682e33a52767647884d3832df8949bf9add338
Esther <estheticallybawo@gmail.com>
2024-08-14 07:20:14 -0700

    Inputed all the details on the Header

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,53 @@
-import React from 'react'
-import { FaHeart } from "react-icons/fa";
 
+"use Client"
+import React from 'react';
+import { TfiMenu } from "react-icons/tfi";
+import Link from 'next/link';
+import { IoSearchSharp } from "react-icons/io5";
+import { PiShoppingCartLight } from "react-icons/pi";
+import { FaRegHeart } from "react-icons/fa";
+import { ImGift } from "react-icons/im";
 
 const page = () => {
   return (
-    <div> <FaHeart /></div>
-     
+    <nav >
+      
+      <div className="w-full max-w-xl flex mx-auto p-20 text-xl">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className='Logo'>
+          <Link href ="/">
+            <img src ="esty logo.png"/>
+          </Link>
+        
+          <h3 className='flex space-x-4'><TfiMenu /> Categories </h3>
+          <Link href ="">
+          <PiShoppingCartLight />
+          </Link>
+          <Link href="">
+          <FaRegHeart />
+          </Link>
+          <Link href="">
+          <ImGift />
+          </Link>
+            <input
+                type="text"
+                className="w-full placeholder-gray-400 text-gray-900 p-4"
+                placeholder="Search"/>
+            <button className="bg-orange-600 text-white p-4"><IoSearchSharp /></button>
+
+            <Link href="">
+            <h3 className='flex space-x-4 '><ImGift /> Gift Mode</h3>
+            </Link>
+           
+            <Link href=""> <h3>Shop Birthday Gifts</h3></Link>
+            <Link href=""> <h3>Home Favorites</h3></Link>
+            <Link href=""> <h3>Fashion Finds</h3></Link>
+            <Link href=""> <h3>Registry</h3></Link>
+        </div>
+  
+      </div>
+      </div>
+      </nav>
 
   )
 }
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,10 +1,22 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { LuMenu } from "react-icons/lu";
+import Link from 'next/link';
 
 const navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+        <div className='Logo'>
+          <Link to ="/">
+            <img src ="esty logo.png"/>
+          </Link>
+        <nav>
+        <LuMenu />
+        </nav>
+      </div>
+
+    </header>
+    
   )
 }
 
