31143bede3aab3c3ac41e45fd5453fd16655ecbf
Esther <estheticallybawo@gmail.com>
2024-11-20 00:10:16 +0100

    Featured Categories

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -5,7 +5,53 @@ import React from "react";
 const Cards = () => {
 return (
      <>
-     
+     <h3>Featured Categories </h3>
+     <div className="flex " >
+          <ul>
+               <li>
+                    <a>
+                         <img src="\Elisha.jpg"width="200px"></img>
+                    </a>
+                    <p>Christmas Ornaments</p>
+               </li>
+          </ul>
+          <ul>
+               <li>
+                    <a>
+                         <img src="\Olivia.jpg"width="200px"></img>
+                    </a>
+                    <p>Signs</p>
+               </li>
+          </ul><ul>
+               <li>
+                    <a>
+                         <img src="\Star.jpg"width="200px"></img>
+                    </a>
+                    <p>Wall Decor</p>
+               </li>
+          </ul><ul>
+               <li>
+                    <a>
+                         <img src="\pup.jpg"width="200px"></img>
+                    </a>
+                    <p>Digital Prints</p>
+               </li>
+          </ul><ul>
+               <li>
+                    <a>
+                         <img src="\painting.jpg"width="200px"></img>
+                    </a>
+                    <p>Digital Prints</p>
+               </li>
+          </ul><ul>
+               <li>
+                    <a>
+                         <img src="\Elisha.jpg"width="200px"></img>
+                    </a>
+                    <p>Christmas Ornaments</p>
+               </li>
+          </ul>
+    </div>
      </>
 )
 }
